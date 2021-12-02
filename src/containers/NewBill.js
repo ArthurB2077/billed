741731debb68e33363c0296791c8bd56ddd42c78
Arthur Béquié <arthurbequie@gmail.com>
2021-12-02 22:38:27 +0100

    OP9-4: controle sur les fichiers importés ajouté au niveau HTML et au niveau NewBills.js

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -16,22 +16,27 @@ export default class NewBill {
     new Logout({ document, localStorage, onNavigate })
   }
   handleChangeFile = e => {
+    const acceptedFormat = ["jpg", "jpeg", "png"]
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
-    this.firestore
-      .storage
-      .ref(`justificatifs/${fileName}`)
-      .put(file)
-      .then(snapshot => snapshot.ref.getDownloadURL())
-      .then(url => {
-        this.fileUrl = url
-        this.fileName = fileName
-      })
+    if (acceptedFormat.filter(format => file.type.includes(format)).length !== 0) {
+      this.firestore
+          .storage
+          .ref(`justificatifs/${fileName}`)
+          .put(file)
+          .then(snapshot => snapshot.ref.getDownloadURL())
+          .then(url => {
+            this.fileUrl = url
+            this.fileName = fileName
+          })
+    } else {
+      this.document.querySelector(`input[data-testid="file"]`).value = ""
+      throw new Error("Le format du fichier n'est pas accepté. Veuillez importer un fichier de type jpg, jpeg ou png")
+    }
   }
   handleSubmit = e => {
     e.preventDefault()
-    console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
     const email = JSON.parse(localStorage.getItem("user")).email
     const bill = {
       email,
